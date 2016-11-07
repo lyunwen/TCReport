2b3c5b07eecf96865db28d42e1b1bda0123ffe99
Administrator <Administrator@CHROWDERLIU-PC0>
2016-11-07 11:18:03 +0800

    gg

--- TCReport.DTO/DBModel/Account_Resourcegroup_Mapping.cs
+++ TCReport.DTO/DBModel/Account_Resourcegroup_Mapping.cs
@@ -3,40 +3,27 @@ using System.Text;
 using System.Collections.Generic;
 using System.Data;
 using TCReport.DTO.AutoCodeAttribute;
-
 namespace TCReport.DTO.DBModel
 {
     [Table("account_resourcegroup_mapping")]
-    public class Account_ResourceGroup_Mapping
+    public class db_account_resourcegroup_mapping
     {
         /// <summary>
-        /// auto_increment
-        /// </summary>		
-        private long _id;
-        public long ID
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
+        /// Type[bigint(20)] Nullable[False]
+        /// </summary>
+        [Key]
+        [AutoCreate]
+        public long ID { get; set; }
+
         /// <summary>
-        /// AccountID
-        /// </summary>		
-        private long _accountid;
-        public long AccountID
-        {
-            get { return _accountid; }
-            set { _accountid = value; }
-        }
+        /// Type[bigint(20)] Nullable[False]
+        /// </summary>
+        public long AccountID { get; set; }
+
         /// <summary>
-        /// ResourceGroupID
-        /// </summary>		
-        private long _resourcegroupid;
-        public long ResourceGroupID
-        {
-            get { return _resourcegroupid; }
-            set { _resourcegroupid = value; }
-        }
+        /// Type[bigint(20)] Nullable[False]
+        /// </summary>
+        public long ResourceGroupID { get; set; }
 
     }
 }
-
--- TCReport.DTO/DBModel/Resource.cs
+++ TCReport.DTO/DBModel/Resource.cs
@@ -3,40 +3,27 @@ using System.Text;
 using System.Collections.Generic;
 using System.Data;
 using TCReport.DTO.AutoCodeAttribute;
-
 namespace TCReport.DTO.DBModel
 {
     [Table("resource")]
-    public class Resource
+    public class db_resource
     {
         /// <summary>
-        /// auto_increment
-        /// </summary>		
-        private long _id;
-        public long ID
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
+        /// Type[bigint(20)] Nullable[False]
+        /// </summary>
+        [Key]
+        [AutoCreate]
+        public long ID { get; set; }
+
         /// <summary>
-        /// Value
-        /// </summary>		
-        private string _value;
-        public string Value
-        {
-            get { return _value; }
-            set { _value = value; }
-        }
+        /// Type[varchar(255)] Nullable[False]
+        /// </summary>
+        public string Value { get; set; }
+
         /// <summary>
-        /// CreateBy
-        /// </summary>		
-        private string _createby;
-        public string CreateBy
-        {
-            get { return _createby; }
-            set { _createby = value; }
-        }
+        /// Type[varchar(255)] Nullable[False]
+        /// </summary>
+        public string CreateBy { get; set; }
 
     }
 }
-
